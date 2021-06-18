f4202503a82d49a0026b8b0f9bef572a427027a7
Dan Mailman <dmailman@gmail.com>
2021-06-17 22:16:54 -0500

    Add tAverager to Utilities

--- Utilities.py
+++ Utilities.py
@@ -10,6 +10,21 @@ def LimitVal(nVal,nLoLim,nHiLim):
 	if nVal < nLoLim: return nLoLim
 	if nVal > nHiLim: return nHiLim
 	return nVal
+class tAverager:
+	def __init__(self):
+		self.nSum = 0
+		self.nCnt = 0
+	def start(self,nNum):
+		self.nSum = nNum
+		self.nCnt = 1
+	def add(self,nNum):
+		self.nSum += nNum
+		self.nCnt += 1
+	def clear(self):
+		self.nSum = 0
+		self.nCnt = 0
+	def GetAvg(self):
+		return self.nSum / self.nCnt
 if __name__ == "__main__":
 	def main():
 		ts1 = dt.now()
@@ -18,4 +33,10 @@ if __name__ == "__main__":
 		print(f"absdiff: {absdiff(3,2)}")
 		print(f"NEAR: {NEAR(2.5,3.5,1)}")
 		print(f'LimitVal: {LimitVal(3,4,10)}')
+		oAverager = tAverager()
+		oAverager.add(1)
+		oAverager.add(3)
+		oAverager.add(5)
+		print(f'Averager: {oAverager.GetAvg()}: Expect 3')
+		
 	main()
